8a7ce0a757239ade8edc53c199d5148ac2a1b556
DeltaRayDzns <102349320+DeltaRayDzns@users.noreply.github.com>
2026-02-11 23:59:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,3 @@ $(document).ready(function(){
 
 })
 
-
-$(document).ready(function() {
-    $('.needs-validation').on('submit', function(event) {
-        var form = $(this);
-
-        if (form[0].checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        
-        form.addClass('was-validated');
-    });
-});
