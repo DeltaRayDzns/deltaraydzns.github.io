5d26e7460621592ad5d6acebb96ac0663b029cc3
DeltaRayDzns <102349320+DeltaRayDzns@users.noreply.github.com>
2026-02-12 00:33:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ $(document).ready(function(){
 
 })
 
+(function() {
+                  'use strict';
+                  window.addEventListener('load', function() {
+                    var forms = document.getElementsByClassName('needs-validation');
+                    
+                    Array.prototype.filter.call(forms, function(form) {
+                      form.addEventListener('submit', function(event) {
+                        
+                        var password = document.getElementById('validationCustom04');
+                        var confirm = document.getElementById('validationCustom05');
+                
+                        if (password && confirm) {
+                          if (password.value !== confirm.value) {
+                            confirm.setCustomValidity("Passwords do not match");
+                          }
+                          else {
+                            confirm.setCustomValidity("");
+                          }
+                        }
+                
+                        if (form.checkValidity() === false) {
+                          event.preventDefault();
+                          event.stopPropagation();
+                        }
+                        else {
+                          alert("Registration Successful!");
+                          form.reset();
+                        }
+                
+                        form.classList.add('was-validated');
+                      }, false);
+                    });
+                  }, false);
+                })();
